753f2e8685ad76bd328226c1edce7abc3708cf2a
Nguyễn Trung Kiên <trungkiennguyen0310@gmail.com>
2024-05-03 12:42:15 +0700

    adding time tracker

---
+++ Scrumdinger/Views/Scrum/MeetingView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Trung Kiên Nguyễn on 3/5/24.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed,
+                                  secondsRemaining: scrumTimer.secondsRemaining,
+                                  theme: scrum.theme)
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
+            }
+        }
+        .padding()
+        .foregroundStyle(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+}
